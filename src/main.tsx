12672546b91486a69a3e16d2199b9426825d0bf9
sakib-devxhub <git-sakib2@devxhub.com>
2025-05-26 02:41:40 +0600

    feat: remove inject

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,6 @@ import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
-import { inject } from "@vercel/analytics";
-
-inject();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
